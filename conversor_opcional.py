06ab3562a9fc16ea519bf965fc11f60af51d0d59
JaviYo_MAGNO <javi.rudo.5@gmail.com>
2022-02-02 15:27:38 -0600

    Conversor opcional para hacer calculo del dolar de diferentes paises

---
+++ conversor_opcional.py
@@ -0,0 +1,36 @@
+menu= """
+Bienvenido al conversor de monedas
+
+
+1 - Pesos Mexicanos.
+2 - Pesos Colombianos.
+3 - Pesos Argentinos.
+
+Elige una opcion:  
+"""
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input ("cuantos pesos MEXICANOS tienes?:")
+    pesos = float(pesos)
+    valor_dolar = 20.58
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $" + dolares + " dolares")
+elif opcion == 2:
+    pesos = input ("cuantos pesos COLOMBIANOS tienes?:")
+    pesos = float(pesos)
+    valor_dolar = 3,932.50
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $" + dolares + " dolares")
+else opcion == 3:
+    pesos = input ("cuantos pesos ARGENTINOS tienes?:")
+    pesos = float(pesos)
+    valor_dolar = 105.16
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $" + dolares + " dolares")
